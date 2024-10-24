65bc0f4045259ada1da10eb4d9b74ffcced2e025
unknown <anhqui04062004@example.com>
2024-10-24 22:09:28 +0700

    upload

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,18 +1,25 @@
 const Course= require('../models/Course');
+const {multipleMongooseToObject}= require('../../util/mongoose');
 
 class SiteController {
     // [GET] /
-    async index(req, res) {
-
-        try{
-            const courses= await Course.find({});
-            res.json(courses);
-            res.render('home');
-        }
-        catch(error){
-            console.error({error: 'ERORR'});
-            process.exit(1);
-        }
+    async index(req, res, next) {
+        // try{
+        //     const courses= await Course.find({});
+        //     res.json(courses);
+        //     res.render('home');
+        // }
+        // catch(error){
+        //     console.error({error: 'ERORR'});
+        //     process.exit(1);
+        // }
+        await Course.find({})
+        .then(courses=>{
+            res.render('home', {
+                courses: multipleMongooseToObject(courses)
+            });
+        })
+        .catch(next);
     }
 
     // [GET] /search
--- src/index.js
+++ src/index.js
@@ -28,10 +28,10 @@ app.use(morgan('combined'));
 //Template engine
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources\\views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
